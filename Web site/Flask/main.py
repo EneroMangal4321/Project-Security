03f92817150429383f068686971629064511e9a8
hadi4t <114092094+hadi4t@users.noreply.github.com>
2022-09-28 10:56:22 +0200

    Alleen benodige code

--- Web site/Flask/main.py
+++ Web site/Flask/main.py
@@ -4,261 +4,68 @@ import hashlib
 import random
 from flask import Flask, url_for, request, redirect, render_template, session, flash
 import mysql.connector as mysql
-
-PS = mysql.connect(
-    host='192.168.0.5',
-    database='PS',
-    user='website',
-    password='w3bm@st3r')
+from .users_module import *
 
 
 app = Flask(__name__)
-app.secret_key = "ABC"
+app.secret_key = secrets.token_bytes(16)
 
-
-#########################################################################################################################
-#                                                                                                                       #
-# The part below this are the fuctions that are used across the website                                                 #
-#                                                                                                                       #
-#########################################################################################################################
-
-
-# function to see if the user that has been filled in already exists
-def user_exists(username):
-    # open db session
-    cursor = PS.cursor()
-    # see if the user exists in the database
-    cursor.execute(f"SELECT username FROM User WHERE username = '%s'" % (username))
-    # fetch the result of the above query
-    fetchresult = cursor.fetchmany()
-    # close db session
-    # if fetchresult[0][0] == username:
-    #     return True
-    # else:
-    #     return False
-    if fetchresult is not None:
-        for row in fetchresult:
-            if row[0] == username:
-                return True
-            else:
-                return False
-    else:
-        return False
-
-
-    # close db session
-    cursor.close()
-
-# function to see if the email that has been filled in already exists  
-def email_exists(email):
-    # open db session
-    cursor = PS.cursor()
-    # make the email into a variable
-    # email = request.form[f'Lusername']
-    cursor.execute(f"SELECT email FROM User WHERE email = '%s'" % (email))
-    # fetch the result of the above query
-    fetchresult = cursor.fetchmany()
-    # Checks if 
-    if fetchresult is not None:
-        for row in fetchresult:
-            if row[0] == email:
-                return True
-            else:
-                return False
-    else:
-        return False
-
-    # if fetchresult[0][0] == email:
-    #     return True
-    # else:
-    #     return False
-    # # close db session
-    cursor.close()
-    
-# function to make a salt
-def make_salt():
-    # makes a random 32 byte number that will be used as salt
-    ALPHABET = "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"
-    chars=[]
-    for i in range(32):
-        chars.append(random.choice(ALPHABET))
-
-    salt = "".join(chars)
-    return salt
-
-# checks if salt 
-def salt_check(salt):
-    # open db session
-    cursor = PS.cursor()
-    # see if the salt is already in the db
-    cursor.execute(f"SELECT salt FROM User WHERE salt = '%s'" % (salt))
-    fetchresult = cursor.fetchone()
-    # checks if salt is in db
-    if fetchresult is not None:
-        for row in fetchresult:
-            if row[0] == salt:
-                return True
-            else:
-                return False
-    else:
-        return False
-    # close db session
-    cursor.close()
-    
-# function to encrypt the password given
-def encrypt(password, salt):
-    # encodes the password
-    plaintext = str(password)+str(salt)
-    # hashes the password with sha256 
-    digest = hashlib.sha256(plaintext.encode())
-    # changes the digest into a hex
-    hex_Hash = digest.hexdigest()
-    # returns the hex hash
-    return hex_Hash
-
-# function to add the user to the db
-def add_new_user_to_database(username, email, encrypt_password, salt):
-    # open db session
-    cursor = PS.cursor()
-    # add user to DB
-    cursor.execute(f"INSERT INTO User (username, email, password, salt) VALUES ('{username}','{email}', '{encrypt_password}', '{salt}');")
-    # commit changes
-    PS.commit()
-    # close db session
-    cursor.close()
-
-
-#########################################################################################################################
-#                                                                                                                       #
-# The part below this is for the Dutch end of the website                                                               #
-#                                                                                                                       #
-#########################################################################################################################
-
-@app.route('/')
+@app.route("/")
 def indexNL():
     return render_template('index-NL.html')
 
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route("/login", methods=['GET', 'POST'])
 def loginNL():
-    cursor = PS.cursor()
-    mail_check = '@'
-    if request.method == f'POST' :       
-        # check if the username filled in is an email or not
-        if mail_check in request.form[f'Lusername']:
-            Lemail = request.form[f'Lusername']
-            Lpassword = request.form[f'Lpassword']
-            # checks if the email exists in the db
-            if email_exists(Lemail) == True:
-                # Get salt of the email in db
-                cursor.execute(f"SELECT salt FROM User WHERE email = '%s'" % (Lemail))
-                salt = cursor.fetchmany()
-                # encrypt the password given with the salt from the db
-                Lpassword = encrypt(Lpassword, salt[0])
-                # get encrypted password from db
-                cursor.execute(f"SELECT password FROM User WHERE email = '%s'" % (Lemail))
-                encrpwd = cursor.fetchmany()
+    #check if user is signed in
+    if 'username' in session:
+        return redirect(url_for('welcome'))
 
-                # check if passwords are the same
-                if Lpassword == encrpwd[0]:
-                    # maakt een cookie aan met de username
-                    session['username'] = request.form['Lemail']
-                    flash(u"You have been logged in", 'info')
-                    return redirect("/")
-                else:
-                    flash("de login gegevens kloppen niet", 'info')
-                    return redirect('/login')
-            else:
-                flash("de login gegevens kloppen niet", 'info')
-                return redirect('/login')
-                
-        
-        else:
-            Lusername = request.form[f'Lusername']
-            Lpassword = request.form[f'Lpassword']
-            # checks if the email exists in the db
-            if user_exists(Lusername) == True:
-                # Get salt of the email in db
-                cursor.execute(f"SELECT salt FROM User WHERE username = '%s'" % (Lusername))
-                salt = cursor.fetchmany()
-                # encrypt the password given with the salt from the db
-                Lpassword = encrypt(Lpassword, salt[0])
-                # get encrypted password from db
-                cursor.execute(f"SELECT password FROM User WHERE username =  '%s'" % (Lusername))
-                encrpwd = cursor.fetchmany()
-                # check if passwords are the same
-                if Lpassword == encrpwd[0]:  
-                    flash(u"You have been logged in")
-                    # maakt een cookie aan met de username                    
-                    session['username'] = request.form['Lusername']
-                    return redirect("/")
-                else:
-                    flash("de login gegevens kloppen niet", 'info')
-                    return redirect('/login')
-            else:
-                flash("de login gegevens kloppen niet", 'info')
-                return redirect('/login')
+    #if user is not signed in let them log in    
     else:
-        return render_template('Login-NL.html')               
+        if request.method == "POST":
+            input_username = request.form["username"]
+            input_username = [input_username]
+            input_passwd = request.form["passwd"]
 
+            status = user_login(input_username, input_passwd)
 
-@app.route('/aanmelden', methods=['GET', 'POST'])
-def aanmelden():
-    error = None;
-    if request.method == f'POST':
-        Susername = request.form[f'Susername']
-        Semail = request.form[f'Semail']
-        Spassword1 = request.form[f'Spassword1']
-        Spassword2 = request.form[f'Spassword2']
-     
-        # check if username and email already exists
-        if user_exists(Susername) == True and email_exists(Semail) == True:
-            flash('De gebruikersnaam en email zijn al in gebruik', 'info')
-            return redirect('/aanmelden')
-        # check if username already exists
-        elif user_exists(Susername) == True:
-            flash('De gebruikersnaam is al in gebruik', 'info')
-            return redirect('/aanmelden')
-        # check if email already exists
-        elif email_exists(Semail) == True:
-            flash('De email adres is al in gebruik', 'info')
-            return redirect('/aanmelden')
-        # check if password is the same as second given password
-        elif Spassword1 != Spassword2:
-            flash("De wachtwoorden komen niet overeen", 'info')
-            return redirect('/aanmelden')
+            # User logged in
+            if status == 0:
+                session['username'] = input_username[0]
+                audit(input_username[0], 2)
+                return redirect(url_for("welcome"))
+
+            # Unsuccesfull log in attempts  
+            elif status == 1:
+                flash("gebruikersnaam en/of wachtwoord incorrect")
+                audit(input_username[0], 3)
+                return render_template("login.html")
+            elif status == 2:
+                flash("gebruikersnaam en/of wachtwoord incorrect")
+                audit(input_username[0], 3)
+                return render_template("login.html")
+
+        #display page if not a post request
         else:
-            # make a salt for the new user
-            salt = make_salt()
-            #check if salt exists
-            if salt_check(salt) == True:
-                salt = make_salt()
-            else:
-                salt = salt
+            return render_template('Login-NL.html')               
 
 
-
-            # encrypt the password with the previously made salt
-            Sencrypt_password = encrypt(Spassword1, salt)
-            # adds the user to the database with username, email and the encrypted password
-            add_new_user_to_database(Susername, Semail, Sencrypt_password, salt)
-            flash(u'Gebruiker is aangemaakt')
-            # maakt een cookie aan met de username      
-            session['username'] = request.form['Susername']
-            return redirect("/")
-    else:
-        return render_template('aanmelden.html') 
-
-
-@app.route('/logout')
+@app.route("/logout")
 def logoutNL():
-    # verwijdert de cookie
-    session['username'] = None
-    return redirect("/")
+    #check if user is logged in
+    if 'username' in session:
+        #delete user from session aka log them out
+        session.pop('username', None)
+        return redirect(url_for('loginNL'))
+
+    #if user not signed in tell them and make them login first
+    else:
+        flash("Om deze pagina te bezoeken moet u ingelogd zijn")
+        return redirect(url_for("loginNL"))
 
 
-
-@app.route('/overons')
+@app.route("/overons")
 def overons():
     return render_template('Overons.html')
 
---
+++ Web site/Flask/users_module.py
@@ -0,0 +1,53 @@
+import mysql.connector as mysql
+import secrets
+import hashlib
+
+#A function that checks users creds. It takes a username and a password, and will return a status.
+# status = 0 means password and username match, status = 1 means username and/or password do not 
+# match and status = 2 means invalid username.
+def user_login(input_username, input_passwd):
+    try:
+        #Establish DB connection with login user.
+        db = mysql.connect(
+        host = "localhost",
+        user = "login",
+        passwd = "*2Sasf@csAas3",
+        database = "psdb")
+
+        #A prepared statement to get username, salt and hashed password from db.
+        mycursor = db.cursor()
+        sql = "SELECT username, salt, password FROM user WHERE username = %s"
+        sql_param = input_username
+        mycursor.execute(sql, sql_param)
+        result = mycursor.fetchone()
+
+        #Assign correct vars to sql result.
+        username = result[0]
+        salt = result[1]
+        password = result[2]
+
+        #Revert username to correct format.
+        input_username = input_username[0]
+        
+        #Hash the password entered by user.
+        input_passwd = input_passwd + salt
+        input_passwd = hashlib.sha256(input_passwd.encode('utf-8')).hexdigest()
+
+        mycursor.close()
+        db.close()
+
+        #check if username and passwords match
+        if input_username == username and input_passwd == password:
+            status = 0
+            return status
+
+        #if username and password dont match display error message 
+        else:
+            status = 1
+            return status
+
+    #if username doesn't exist an error message will be displayed
+    except TypeError:
+
+            status = 2
+            return status 
